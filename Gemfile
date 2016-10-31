5c0ff70bb6538627ecd0787c68edebe25ac87f60
Latoya Watson <latoyamwatson@gmail.com>
2016-10-31 14:48:22 -0400

    new seeds

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem 'figaro'
+# hide api key
 gem 'rack-cors', :require => 'rack/cors'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@
 User.destroy_all
 Event.destroy_all
 
+Lesbians = Event.create!({title: 'Lesbians Who Tech Meetup', code: 'lez connect', location: 'Phase 1', time: '8.00', cost: '5.00', date: '11/2/2016'})
+Disney = Event.create!({title: 'Disney Meetup', code: 'meetup squirrel', location: 'Disneyworld', time: '7.00', cost: '100.00', date: '11/8/2016'})
 
-
-User.create(name: 'Mickey Mouse', photo_url: 'http://cartoon-characters.com/wp-content/uploads/2014/04/Mickey-Mouse-with-Gloves.jpg', linkedin_url: 'https://www.linkedin.com/in/andres-espinoza', hiring:true, looking:false, skills:'bowling', desired_skills: 'carpet flying')
-Event.create(title: 'Disney Meetup', code: 'meetup squirrel', location: 'Disneyworld', time: '7.00', cost: '100.00', date: '11/8/2016')
+Mickey = User.create!({name: 'Mickey Mouse', photo_url: 'http://cartoon-characters.com/wp-content/uploads/2014/04/Mickey-Mouse-with-Gloves.jpg', linkedin_url: 'https://www.linkedin.com/in/andres-espinoza', hiring:true, looking: false, skills:'bowling', desired_skills: 'carpet flying', event: Disney})
+Latoya = User.create!({name: 'Latoya Watson', photo_url: '', linkedin_url: 'https://www.linkedin.com/in/watsonlm', hiring: false, looking: true, skills: 'ruby on rails', desired_skills: 'being a boss at css', event: Lesbian})
