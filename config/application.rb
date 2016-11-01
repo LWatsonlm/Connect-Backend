d88771b62290889a0b9ed05be822465bac5fe837
Latoya Watson <latoyamwatson@gmail.com>
2016-11-01 15:57:56 -0400

    updated rails api

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Tatiana
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers =>  :any, :methods => [:get, :post, :options]
+        resource '*', :headers =>  :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
   end
